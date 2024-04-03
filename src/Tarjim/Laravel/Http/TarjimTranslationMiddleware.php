d6c6ada6e2577c62c88b2f388a0a82257f07e560
jp <jean-paul@panda7.ca>
2024-04-03 20:18:30 +0000

    FIX spacing

--- src/Tarjim/Laravel/Http/TarjimTranslationMiddleware.php
+++ src/Tarjim/Laravel/Http/TarjimTranslationMiddleware.php
@@ -9,27 +9,27 @@ use Joylab\TarjimPhpClient\TarjimClient;
 
 class TarjimTranslationMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     *
-     */
-    public function handle(Request $request, Closure $next)
-    {
-			## Set translation keys
-      $language = 'en';
-      if (Session::has('locale')) {
-        $language = Session::get('locale');
-      }
-
-			$tarjim_config = config('tarjim.path');
-			$Tarjim = new TarjimClient($tarjim_config);
-			$Tarjim->setTranslations($language);
-
-      //if (Session::has('locale')) {
-      //  App::setLocale(Session::get('locale'));
-      //}
-
-      return $next($request);
+  /**
+   * Handle an incoming request.
+   *
+   *
+   */
+  public function handle(Request $request, Closure $next)
+  {
+    ## Set translation keys
+    $language = 'en';
+    if (Session::has('locale')) {
+      $language = Session::get('locale');
     }
+
+    $tarjim_config = config('tarjim.path');
+    $Tarjim = new TarjimClient($tarjim_config);
+    $Tarjim->setTranslations($language);
+
+    //if (Session::has('locale')) {
+    //  App::setLocale(Session::get('locale'));
+    //}
+
+    return $next($request);
+  }
 }
